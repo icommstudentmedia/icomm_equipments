296d9c9f526100639bd0f254032cd949942e2e8d
Isaac Andrade <isaac.nic@gmail.com>
2013-09-19 19:53:20 -0600

    create index.php with some html as a skeleton

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+
+<head></head>
+
+<body>
+	<header>
+
+		<div id="nav-menu">
+			<nav>
+				<ul>
+					<li>Admin Panel</li>
+					<li>New Equipment Request</li>
+					<li>New Facilities Request</li>
+					<li>Logout</li>
+				</ul>
+			</nav>
+		</div>
+
+		<div id="nav-tabs">
+			<nav>
+				<ul>
+					<li>Submitted</li>
+					<li>Approved</li>
+					<li>Rejected</li> <!-- Subcategories: Not Authorizes, No Equipment -->
+					<li>Prepared</li> <!-- Prepped -->
+					<li>Picked-up</li>
+					<li>Returned</li>
+				</ul>
+			</nav>
+		</div>
+
+		<div id="data-table">
+			<table>
+				<tr>
+					<th>Date Created</th>
+					<th>Reporter Name</th>
+					<th>Group</th>
+					<th>Requested Equipment</th>
+					<th>Chackout Date</th>
+					<th>Checkout Time</th>
+					<th>Checkin Date</th>
+					<th>Checkin Time</th>
+					<th>Comment</th>
+					<th>Status</th>
+				</tr>
+			</table>
+		</div>
+
+	</header>
+	<footer></footer>
+</body>
+
+</html>
