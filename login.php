01a582376179bdade8fd640a346af961bb098899
RitchieFitz <ritchiefitz1@gmail.com>
2013-09-25 10:38:53 -0600

    I built a small login page. Just looks not functioning yet.

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="style/screen.css">
+		<link rel="stylesheet" type="text/css" href="style/login.css">
+	</head>
+	<body>
+		<div class="page-wrapper">
+			<label for="username">Username:</label>
+			<input name="username" type="text">
+			<div class="clear-fix"></div>
+			<br>
+			<label for="password">Password:</label>
+			<input name="password" type="password">
+			<div class="clear-fix"></div>
+			<br>
+			<div class="controls">
+				<input name="submit" class="button" type="submit" value="Login">
+				<input type="reset" class="button" value="Clear">
+			</div>
+		</div>
+	</body>
+<?php
+
+?>
